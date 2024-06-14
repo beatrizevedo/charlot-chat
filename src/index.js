bd17dee8801753b2934da6493c09b14fa78fe250
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-06-14 14:36:05 +0100

    Added axios and ai api integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-function generateCharlotAnswer(event) {
-  event.preventDefault();
-
+function displayAnswer(response) {
   new Typewriter('#answer', {
-    strings: 'Meow meow meow meow',
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
+}
+
+function generateCharlotAnswer(event) {
+  event.preventDefault();
+
+  let textForm = document.querySelector('#text-form');
+
+  let apiKey = '052t6596f0376314513oae46624edf8b';
+  let prompt = `User question about cats: ${textForm.value}`;
+  let context =
+    'You are a AI that knows everything about cats but nothing about other things. Make sure to answer all the user questions about cats. If it is about other things, please answer that you do not know the answer.';
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAnswer);
 }
 
 let chatForm = document.querySelector('#chat-form');
