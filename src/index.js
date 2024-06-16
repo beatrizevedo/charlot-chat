bd5395d1bba3828572cda99cc9b84e14637b9adf
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-06-16 13:09:46 +0100

    Added some Charlot context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateCharlotAnswer(event) {
   let apiKey = '052t6596f0376314513oae46624edf8b';
   let prompt = `User question about cats: ${textForm.value}`;
   let context =
-    'You are a AI that knows everything about cats but nothing about other things. Make sure to answer all the user questions about cats. If it is about other things, please answer that you do not know the answer.';
+    'You are a AI that knows everything about cats but nothing about other things. You also know everything about a cat named Charlot. He is a male black and white cat, borned in 2019, with a black moustache. He has a cat sister named Olivia and a human sister called Francisca. He lives in Portugal. He loves water, sleep and do some naughty things, like opening cabinets or jump on top of mirrors. Make sure to answer all the user questions about cats and Charlot. If it is about other things, please answer that you do not know the answer. Give the user small and direct answers.';
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayAnswer);
